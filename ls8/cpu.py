eab5bd401e1e4b82b94a940ed5494fca6b27ea5e
Westley Feller <feller.westley@gmail.com>
2020-10-04 09:21:12 -0700

    Un-hardcoded program instructions
    Instructions are read from a file based on command line arguments
    supplied at run time. Only valid instructions are read by filtering out
    comments and blank lines.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -61,22 +61,19 @@ class CPU:
             print("Error: MAR out of bounds")
             return -1
 
-    def load(self):
+    def load(self, ins_file: str):
         """Load a program into memory."""
+        program = []
+        try:
+            with open(ins_file) as f:
+                for line in f:
+                    if line[0] not in ["#", "\n"]:
+                        program.append(int(line[:8], 2))
+        except FileNotFoundError:
+            print(f"Error: {ins_file} not found")
+            return -1
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
 
         for instruction in program:
             self.ram[address] = instruction
